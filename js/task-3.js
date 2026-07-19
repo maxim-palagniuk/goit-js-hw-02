2695c6065aaacd78022c7ae1836067b6da2de9aa
b1zecs <maxlolkill@gmail.com>
2026-07-19 20:59:47 +0300

    2 hw js complete

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+function checkForSpam(message) {
+    let lowerCaseMessage = message.toLowerCase()
+
+    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+        return true
+    }
+
+    else {
+        return false
+    }
+}
+
+checkForSpam( "Latest technology news" )
+checkForSpam( "JavaScript weekly newsletter" )
+checkForSpam( "Get best sale offers now!" )
+checkForSpam( "Amazing SalE, only tonight!" )
+checkForSpam( "Trust me, this is not a spam message" )
+checkForSpam( "Get rid of sPaM emails. Our book in on sale!" )
+checkForSpam( "[SPAM] How to earn fast money?")
+
+
+console . log (checkForSpam( "Latest technology news" )); // false 
+console . log (checkForSpam( "JavaScript weekly newsletter" )); // false 
+console . log (checkForSpam( "Get best sale offers now!" )); // true 
+console . log (checkForSpam( "Amazing SalE, only tonight!" )); // true 
+console . log (checkForSpam( "Trust me, this is not a spam message" )); // true 
+console . log (checkForSpam( "Get rid of sPaM emails. Our book in on sale!" )); // true 
+console . log (checkForSpam( "[SPAM] How to earn fast money?" )); // true
